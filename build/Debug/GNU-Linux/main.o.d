3d5c262fde241d63671ae300a71d25b9b38ae471
ie04 <iyadeltifi12@gmail.com>
2018-12-01 17:11:37 -0500

    Added Project Files

---
+++ build/Debug/GNU-Linux/main.o.d
@@ -0,0 +1,16 @@
+build/Debug/GNU-Linux/main.o: main.c score.h average.h scan.h constants.h \
+ error.h not_equals_yno.h add_array_numbers.h
+
+score.h:
+
+average.h:
+
+scan.h:
+
+constants.h:
+
+error.h:
+
+not_equals_yno.h:
+
+add_array_numbers.h:
