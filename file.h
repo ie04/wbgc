2902e99b37d4d8d7e9586eac8011fbd123420e4c
ie04 <iyadeltifi12@gmail.com>
2018-12-01 19:47:37 -0500

    added file.h

---
+++ file.h
@@ -0,0 +1,14 @@
+
+/* 
+ * File:   file.h
+ * Author: ie04
+ *
+ * Created on December 1, 2018, 7:01 PM
+ */
+
+#ifndef FILE_H
+#define FILE_H
+//This header file will implement file I/O in wbgc
+
+#endif /* FILE_H */
+
